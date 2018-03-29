a8bf6ade2e3b46139bdc042199b9ca38f90ddcdf
Naeem Akhtar <momin.naeemakhtar@gmail.com>
2018-03-29 18:14:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { WeatherComponent } from './weather/weather.component';
+import { MovieComponent } from './movie/movie.component';
+import { CONST_ROUTING } from './app.routing';
+import { SharedService } from './shared.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrencyComponent,
+    WeatherComponent,
+    MovieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CONST_ROUTING
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { CONST_ROUTING } from './app.routing';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: []
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
